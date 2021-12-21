b1c2bd4b028667441309e5d858046fac90aab636
HommaKai <81701733+HommaKai@users.noreply.github.com>
2021-12-21 14:13:17 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectWebApplication.Startup))]
+namespace ProjectWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
